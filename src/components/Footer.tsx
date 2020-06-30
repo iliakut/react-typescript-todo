82952db5a738de2a9bc990108adb1d70fdf36835
ilia <ilyakut@gmail.com>
2020-07-01 00:26:40 +0300

    added filtering todoItems

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { Stack, Text, DefaultButton  } from "@fluentui/react";
+import {Stack, Text} from "@fluentui/react";
+import {useSelector} from "react-redux";
+import {ItodoItem} from "../store/constants/typesTodo";
+import {StoreType} from "../store/store";
 
 const Footer = () => {
+  const items: ItodoItem[] = useSelector((state: StoreType) => state.todoStore.todos);
+
   return (
     <Stack horizontal horizontalAlign="space-between" verticalAlign="center">
       <Text>
         3 items left
       </Text>
-      <DefaultButton onClick={() => console.log(1)}>Clear Completed</DefaultButton>
     </Stack>
   );
 };
