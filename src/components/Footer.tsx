fae4cf59fbffff91ee4124a1e6f3a2c1697f5efe
ilia <ilyakut@gmail.com>
2020-07-01 16:55:32 +0300

    added prevent rendering in Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Stack, Text} from "@fluentui/react";
 import {useSelector} from "react-redux";
 import {ItodoItem} from "../store/constants/typesTodo";
 import {StoreType} from "../store/store";
 
 const Footer = () => {
-  const items: ItodoItem[] = useSelector((state: StoreType) => state.todoStore.todos);
+  const items: ItodoItem[] = useSelector(
+    (state: StoreType) => state.todoStore.todos,
+    (newItems, oldItems): boolean => {
+      return newItems.length === oldItems.length;
+    });
+
+  const length = items.length;
 
   return (
     <Stack horizontal horizontalAlign="space-between" verticalAlign="center">
       <Text>
-        3 items left
+        {length} items left
       </Text>
     </Stack>
   );
